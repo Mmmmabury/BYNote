2f4ae2781fb14c2f19641a2732a3757db70f5629
Mmmmabury <cbylalala@gmail.com>
2016-06-22 14:53:06 +0800

    完善了指纹锁，添加了启动页面

---
+++ BYNote/LaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.h
+//  BYNote
+//
+//  Created by cby on 16/6/22.
+//  Copyright © 2016年 cby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchViewController : UIViewController
+
+@end
---
+++ BYNote/LaunchViewController.m
@@ -0,0 +1,40 @@
+//
+//  LaunchViewController.m
+//  BYNote
+//
+//  Created by cby on 16/6/22.
+//  Copyright © 2016年 cby. All rights reserved.
+//
+
+#import "LaunchViewController.h"
+
+@interface LaunchViewController ()
+
+@end
+
+@implementation LaunchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)viewTap:(UIControl *)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
